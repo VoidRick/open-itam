04706cd9403253452aa43e3ad9cb3b42b3d91e6f
VoidRick <neonvoidrick@gmail.com>
2026-07-21 16:02:46 +0300

    fix(style): Исправлены проблемы в стилях меню навигации
    Signed-off-by: VoidRick <neonvoidrick@gmail.com>

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,7 +4,7 @@ const drop_menu = document.querySelector("a.drop_menu");
 // Отслеживаем нажатие на элемент и выполняем функцию
 drop_menu.addEventListener("click", function(){
     // Идём "выше" и получаем "родительский" li
-    const menuItem = drop_menu.closest("li").querySelector(".dropdownMenu");
+    const menuItem = drop_menu.closest("li").querySelector(".dropDownMenu");
 
     // Добавляем или убираем класс "open"
     menuItem.classList.toggle("hidden")
